19826e5a4ac09e42c801cab5af1b5b80a5fa7595
charsyam <charsyam@gmail.com>
2018-01-29 15:30:55 +0900

    add failure test

--- python/tests/test_utils2.py
+++ python/tests/test_utils2.py
@@ -9,6 +9,9 @@ class TestUtils(unittest.TestCase):
 
     def test_add_1_1(self):
         self.assertEqual(2, utils2.add(1,1))
+    
+    def test_add_1_2_fail(self):
+        self.assertEqual(2, utils2.add(1,2))
 
 if __name__=="__main__":
     sys.exit(unittest.main())
